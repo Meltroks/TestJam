7338abfb434e1e17f3a0ae637ae7727411942b10
Vad'OS <big-vadim@mail.ru>
2016-03-05 20:36:34 +0300

    moving script for rpg

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float maxLifes,lifes;
+	public float speed;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		//moving shit
+		move();
+		
+	}
+	void move(){
+		transform.Translate(transform.right*Input.GetAxis("Horizontal")*speed*Time.deltaTime + transform.up*Input.GetAxis("Vertical")*speed*Time.deltaTime);
+	}
+}
