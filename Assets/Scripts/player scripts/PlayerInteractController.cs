9fa9fce921bead89a31477f39ebcc1a64c005895
Vad'OS <big-vadim@mail.ru>
2016-03-07 13:16:24 +0300

    еще доработочки

---
+++ Assets/Scripts/player scripts/PlayerInteractController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInteractController : MonoBehaviour {
+    public ActionController actCont;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.GetComponent<Item>() != null)
+        {
+            actCont.getItem(col.GetComponent<Item>());
+        }
+    }
+}
---
+++ Assets/Scripts/player scripts/PlayerInventoryController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInventoryController : MonoBehaviour {
+    public Item[] items;
+    public int[] cols;
+    public void putItem(Item item)
+    {
+        for(int i = 0; i < items.Length; i++)
+        {
+            if (item.name == items[i].name && item.texture == items[i].texture)
+            {
+                cols[i]++;
+                
+            }
+        }
+    }
+}
